fc744d5ece2e7468173db488e0fe4adcbcfc1a16
jonghyeon <whdgus003@gmail.com>
2023-02-12 20:08:08 +0900

    feat(root-scpoe): init
    develop 브랜치 추가 및 프로젝트 초기화

---
+++ src/main/java/com/example/challeculumbackend/ChalleculumBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.challeculumbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChalleculumBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ChalleculumBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/challeculumbackend/ChalleculumBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.challeculumbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChalleculumBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
