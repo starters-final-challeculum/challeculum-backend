1a22bb919edf2fe7fc89a409b02c682594b04b77
joons5834 <25703159+joons5834@users.noreply.github.com>
2023-02-16 16:37:22 +0900

    Fix(userground): Users can only view their own grounds and admins can view anyone's grounds

--- src/main/java/companion/challeculum/domains/ground/GroundController.java
+++ src/main/java/companion/challeculum/domains/ground/GroundController.java
@@ -1,8 +1,12 @@
 package companion.challeculum.domains.ground;
 
+import companion.challeculum.security.PrincipalDetails;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.http.HttpStatus;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Map;
@@ -29,7 +33,17 @@ public class GroundController {
     @GetMapping("/api/v1/userground/{userId}")
     List<Map<String, Object>> getMyGrounds(@PathVariable long userId,
                                            @RequestParam int page,
-                                           @RequestParam(required = false) String status) {
+                                           @RequestParam(required = false) String status,
+                                           Authentication authentication) {
+        if(authentication == null){
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "로그인하지 않았습니다.");
+        }
+        long sessUserId = ((PrincipalDetails) authentication.getPrincipal()).getUser().getId();
+        boolean hasAdminRole = authentication.getAuthorities().stream()
+                .anyMatch(r -> r.getAuthority().equals("ROLE_ADMIN"));
+        if(sessUserId != userId && !hasAdminRole){
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "본인이나 관리자만 확인 가능합니다.");
+        }
         return service.getMyGrounds(userId, page, status);
     }
 
