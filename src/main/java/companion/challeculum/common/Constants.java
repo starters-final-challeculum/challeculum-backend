06537e3cf3ef57014c17ce81ad38e9355a91c43a
jonghyeon <whdgus003@gmail.com>
2023-02-19 05:40:07 +0900

    feat : 데이터베이스에 이용할 상수(Constants) 정의

--- src/main/java/companion/challeculum/common/Constants.java
+++ src/main/java/companion/challeculum/common/Constants.java
@@ -8,4 +8,11 @@ public class Constants {
     public static final String ROLE_MEMBER = "ROLE_MEMBER";
     public static final String ROLE_ADMIN = "ROLE_ADMIN";
     public static final int ROWS_PER_PAGE = 10;
+    public static final String GROUND_STATE_PENDING = "pending";
+    public static final String GROUND_STATE_WAITING = "waiting";
+    public static final String GROUND_STATE_ONGOING = "ongoing";
+    public static final String GROUND_STATE_COMPLETED = "completed";
+    public static final String USER_MISSION_STATE_WAITING = "waiting";
+    public static final String USER_MISSION_STATE_REJECTED = "rejected";
+    public static final String USER_MISSION_STATE_ACCEPTED = "accepted";
 }
