cccc9b58fe690fd369b809ff354b576910a268b4
HyunJoonCho92 <9hjcho2@gmail.com>
2023-02-23 13:59:38 +0900

    feat : 인증된 유저(로그인 상태)가 등록한 강의 제외한 강의 목록 조회(Search) api 추가

--- src/main/java/companion/challeculum/common/Constants.java
+++ src/main/java/companion/challeculum/common/Constants.java
@@ -8,8 +8,8 @@ public class Constants {
     public static final String ROLE_MEMBER = "ROLE_MEMBER";
     public static final String ROLE_ADMIN = "ROLE_ADMIN";
     public static final int ROWS_PER_PAGE = 10;
-    public static final String GROUND_STATE_PENDING = "pending";
-    public static final String GROUND_STATE_WAITING = "waiting";
+    public static final String GROUND_STATE_PENDING = "waiting";
+    public static final String GROUND_STATE_WAITING = "standby";
     public static final String GROUND_STATE_ONGOING = "ongoing";
     public static final String GROUND_STATE_COMPLETED = "completed";
     public static final String USER_MISSION_STATE_WAITING = "waiting";
--- src/main/java/companion/challeculum/domains/user/UserDao.java
+++ src/main/java/companion/challeculum/domains/user/UserDao.java
@@ -27,4 +27,5 @@ public interface UserDao {
 
     Optional<User> findByOAuthId(String oauthId);
     void deleteUser(Long userId);
+
 }
